9d092230e81d46fec57edf898232725e7a61278a
jotavgalves <jvgacontato@gmail.com>
2026-09-09 13:56:24 -0300

    Usa nova arte ilustrada do Palhaço Carmesim na introdução (#18)
    * Usa nova ilustração completa do Palhaço Carmesim na introdução
    
    * Atualiza cache da nova arte introdutória
    
    * Adiciona arte final ilustrada do guichê do Palhaço Carmesim
    
    * Atualiza cache para a nova introdução ilustrada

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -54,7 +54,7 @@ export async function onRequest(context) {
   const transformed = rewriter
     .on("body", {
       element(element) {
-        element.append('<script type="module" src="/assets/js/boot.js?v=20260909-11"></script>', { html: true });
+        element.append('<script type="module" src="/assets/js/boot.js?v=20260909-12"></script>', { html: true });
       }
     })
     .transform(response);
