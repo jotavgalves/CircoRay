08d6d03680ae1bafb654f210d4ac47cb08a1997b
jotavgalves <jvgacontato@gmail.com>
2026-09-10 10:41:08 -0300

    Renova cache do boot para swap Fúria v3

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -54,7 +54,7 @@ export async function onRequest(context) {
   const transformed = rewriter
     .on("body", {
       element(element) {
-        element.append('<script type="module" src="/assets/js/boot.js?v=20260910-10"></script>', { html: true });
+        element.append('<script type="module" src="/assets/js/boot.js?v=20260910-11"></script>', { html: true });
       }
     })
     .transform(response);
