6eb7aa3017921d53e6e41eea79ba4a0eceac99d9
jotavgalves <jvgacontato@gmail.com>
2026-09-10 21:44:13 -0300

    Renova boot após correção do toque mobile

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -54,7 +54,7 @@ export async function onRequest(context) {
   const transformed = rewriter
     .on("body", {
       element(element) {
-        element.append('<script type="module" src="/assets/js/boot.js?v=20260910-17"></script>', { html: true });
+        element.append('<script type="module" src="/assets/js/boot.js?v=20260910-18"></script>', { html: true });
       }
     })
     .transform(response);
